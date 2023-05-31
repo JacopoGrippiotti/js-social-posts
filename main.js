1d692447be1534b87fc451893d5988c4cbd1f036
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-31 16:59:31 +0200

    fixed script.js file to start structuring element to append to container

--- main.js
+++ main.js
@@ -87,3 +87,56 @@ const posts = [
             </div>            
         </div>*/
 
+function calcTimePassed(date){
+
+    let timePassed = Date.parse(date) 
+
+}
+const container = document.getElementById('container')
+
+posts.forEach(post => {
+
+    const {id,content,media,author,likes,created} = post 
+
+    const authorImage = author.image
+
+    const authorName = author.name
+    
+    let newPost = document.createElement('div')
+
+    newPost.classList.add("post")
+
+    newPost.innerHTML +=
+    `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${authorImage} alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${authorName}</div>
+                <div class="post-meta__time"></div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+    <div class="post__image">
+        <img src=${media} alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid=${id}>
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div>                 
+    `
+});
+
